f7d671d971b9699ec14f8a3a42a7367898c5d003
lautarogregorat <lautarogregorat@gmail.com>
2024-12-10 16:52:05 -0300

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('data', (req, res) => JSON.stringify(req.body));
 app.use(
     morgan(':method :url :status :res[content-length] - :response-time ms :data')
