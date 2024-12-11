ffa26432299029d91a84dd2ff01933afc7f99ef7
lautarogregorat <lautarogregorat@gmail.com>
2024-12-11 10:26:54 -0300

    ready

---
+++ models/person.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: false,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v) && v.split('-')[1].length >= 5
+      },
+      message: props => `${props.value} is not a valid phone number!`,
+    },
+  },
+})
+
+personsSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('person', personsSchema)
